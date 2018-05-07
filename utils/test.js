c6509eb2ef62cb4bf2b42daa1ba3294b8f695d5f
entropy <alexandrisanas@gmail.com>
2018-05-07 11:23:31 +0300

    init

---
+++ utils/test.js
@@ -0,0 +1,13 @@
+// const summer = (() => console.log("the call back is here")) => {
+//     console.log("sumeer is running");
+//     callback();
+// }
+
+
+x = "hello".number
+console.log(x)
+// const winder = () => {
+//     console.log("callbackk is running");
+// }
+
+// summer(winder);
