a7ef6a38fa690ded44da10f8fa0051b0598ea7e6
entropy <alexandrisanas@gmail.com>
2018-05-22 23:36:13 +0300

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,14 @@ import './Assets/css/default.min.css';
 // import './Assets/css/bootstrap.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    }
+  }
+
   render() {
     return (
       <Router>
