cc0abf077929c98f209080de73fa5cf7694f70d5
entropy82 <froutopotos.alex@gmail.com>
2018-06-08 11:23:09 +0300

    work

--- routes/imageUploader.js
+++ routes/imageUploader.js
@@ -8,12 +8,12 @@ const upload = require("../services/imageService");
 module.exports = app => {
 
         app.post('/addimage', upload.single('sampleImage'), (req, res) => {
-        console.log(req.file);
+        // console.log(req.file);
         const imageUrl = req.file.path
                     .split('\\').join('/');
         // console.log(imageUrl);
 
-        let sql = `INSERT INTO images (url, reg_date)
+        let sql = `INSERT INTO images (url, created) 
                    VALUES ('${imageUrl}', NOW())`
 
         let query = db.query(sql, (err, result) => {
