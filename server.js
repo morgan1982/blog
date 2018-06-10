5c174be87f393c598be79c971c257dc10d68b021
entropy <alexandrisanas@gmail.com>
2018-06-11 01:00:25 +0300

    admin2

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-
+const path = require('path');
 
 
 // MIDDLEWARE
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use('/images', express.static(path.join(__dirname, "public/uploads")))
 // for images storage
 // app.use(bodyparser({keepExtensions: true, uploadDir: __dirname + '/client/images'})) // folder outside assets is used
 
