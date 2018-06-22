cea48524b76d397bee71cd23a6c81778ceaba52a
entropy82 <froutopotos.alex@gmail.com>
2018-06-22 12:35:21 +0300

    grid ex 1

--- client/src/components/test/testing.js
+++ client/src/components/test/testing.js
@@ -2,20 +2,16 @@ import React from 'react';
 
 export default () => (
 
+<div>
+    <h1>Css Grid</h1>
     <div className="testContainer">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 col-lg-6 col-sm-12">
-                    <div className="card">
-                        <div className="card-body">
-                        <h3 className="card-title">hello there</h3>
-                            <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam temporibus, ducimus dolore dignissimos amet explicabo similique, impedit odit nobis molestias sapiente earum? Delectus asperiores praesentium quasi? A magni sapiente dignissimos quia accusantium! Culpa nostrum quasi natus, voluptatibus sapiente vero minus.</p>
-                            <div className="btn btn-primary">read more</div>
-                        </div>
-                    </div>
-                </div>            
-            </div>
-        </div>
-    </div>
+      <div className="box box-1">One</div>
+      <div className="box box-2">Two</div>
+      <div className="box box-3">Three</div>
+      <div className="box box-4">Four</div>
+      <div className="box box-5">Five</div>
+    </div>    
+</div>
+
 
 )
