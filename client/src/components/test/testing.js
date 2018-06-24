64d085476ce5938b2a12bf01754a24ec2498c1cd
entropy <alexandrisanas@gmail.com>
2018-06-24 21:18:51 +0300

    init

--- client/src/components/test/testing.js
+++ client/src/components/test/testing.js
@@ -5,29 +5,17 @@ import { Link } from 'react-router-dom';
 
 export default () => (
 
-    <div className="testContainer">
-
-        <div className="sidebar">
-            <div className="logo">Awesome<span>Portofolio</span></div>
-            <nav>
-                <Link className="nav-item" to="/admin">Home</Link>
-                <Link className="nav-item" to="/admin">About</Link>
-                <Link className="nav-item" to="/admin">Portofolio</Link>
-                <Link className="nav-item" to="/admin">Contact</Link>
-            </nav>
-        </div>
-
-        <div className="main-content">
-            <div className="portofolio">
-                <div className="portofolio-item medium">one</div>
-                <div className="portofolio-item large two">two</div>
-                <div className="portofolio-item medium">three</div>
-                <div className="portofolio-item small">four</div>
-                <div className="portofolio-item tall">five</div>
-                <div className="portofolio-item wide">six</div>
-            </div>
-        </div>
-
+    <div className="container">
+        <div className="item">1</div>
+        <div className="item">2</div>
+        <div className="item">3</div>
+        <div className="item">4</div>
+        <div className="item">5</div>
+        <div className="item">6</div>
+        <div className="item">7</div>
+        <div className="item">8</div>
+        <div className="item">9</div>
+        <div className="item">10</div>
     </div>
 
 )
