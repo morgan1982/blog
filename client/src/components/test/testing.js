1838cba7d82069d288dad6997d291399ea6ea784
entropy <alexandrisanas@gmail.com>
2018-06-25 00:56:10 +0300

    init

--- client/src/components/test/testing.js
+++ client/src/components/test/testing.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default () => (
 
     <div className="container">
-        <div className="item">1</div>
+        <div className="item one">1</div>
         <div className="item">2</div>
         <div className="item">3</div>
         <div className="item">4</div>
