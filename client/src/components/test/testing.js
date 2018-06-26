a19c61a603184ed1448d1192b8105cf7190e9b84
entropy <alexandrisanas@gmail.com>
2018-06-27 02:32:04 +0300

    adjusting the areas

--- client/src/components/test/testing.js
+++ client/src/components/test/testing.js
@@ -5,19 +5,19 @@ import { Link } from 'react-router-dom';
 
 export default () => (
 
-    <div className="container">
+    <div className="post">
         <h1 className="title">the future of node.js</h1>
         <div className="grid-container">
-            <div className="item one">1</div>
-            <div className="item two">2</div>
-            <div className="item">3</div>
-            <div className="item">4</div>
+            <div className="item hero">1</div>
+            <div className="item content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo hic voluptate, quas maiores quae officia porro, id reprehenderit quo vero suscipit neque nam, nostrum laborum numquam. Recusandae excepturi eius reprehenderit architecto perferendis fugiat accusamus facere ipsum, fugit quod nesciunt beatae laboriosam nostrum optio autem cumque molestias a totam vitae, dolorem labore. Doloribus incidunt voluptate alias culpa officiis numquam blanditiis accusantium perferendis amet est, veniam hic quos quasi, molestiae ab nulla modi quam, autem quo! Excepturi accusamus laboriosam aut quam, doloribus sed magnam velit illo itaque, eaque delectus adipisci voluptate a repellendus, sapiente, ut cum quisquam. Nam repellendus minima rerum sapiente.</div>
+            <div className="item image-1">3</div>
+{/*            <div className="item">4</div>
             <div className="item">5</div>
             <div className="item">6</div>
             <div className="item">7</div>
             <div className="item">8</div>
             <div className="item">9</div>
-            <div className="item">10</div>
+            <div className="item">10</div>*/}
         </div>
     </div>
 
