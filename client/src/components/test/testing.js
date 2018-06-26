0714d5aff6fb0adc598ea70ba7c180912efc02e5
entropy <alexandrisanas@gmail.com>
2018-06-26 21:02:20 +0300

    add hero

--- client/src/components/test/testing.js
+++ client/src/components/test/testing.js
@@ -6,17 +6,21 @@ import { Link } from 'react-router-dom';
 export default () => (
 
     <div className="container">
-        <div className="item one">1</div>
-        <div className="item">2</div>
-        <div className="item">3</div>
-        <div className="item">4</div>
-        <div className="item">5</div>
-        <div className="item">6</div>
-        <div className="item">7</div>
-        <div className="item">8</div>
-        <div className="item">9</div>
-        <div className="item">10</div>
+        <h1 className="title">the future of node.js</h1>
+        <div className="grid-container">
+            <div className="item one">1</div>
+            <div className="item two">2</div>
+            <div className="item">3</div>
+            <div className="item">4</div>
+            <div className="item">5</div>
+            <div className="item">6</div>
+            <div className="item">7</div>
+            <div className="item">8</div>
+            <div className="item">9</div>
+            <div className="item">10</div>
+        </div>
     </div>
+
 
 )
 
